bbcb42d79cb0c842a8b861f896a765262929876d
TilmanK <tilman.krummeck@googlemail.com>
2021-10-12 10:25:30 +0200

    Removed line breaks. Fixes overloading with static methods.

--- generate_upstream.py
+++ generate_upstream.py
@@ -7,6 +7,7 @@ import tempfile
 import zipfile
 
 from libcst import parse_module
+from mypy import api
 
 from fixes.signal_fixer import SignalFixer
 
@@ -17,9 +18,7 @@ except FileExistsError:
     pass
 
 # Update pip just in case
-subprocess.check_call(
-    [sys.executable, "-m", "pip", "install", "--upgrade", "pip"]
-)
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
 
 # Download required packages
 with tempfile.TemporaryDirectory() as temp_dwld_folder:
@@ -41,9 +40,7 @@ with tempfile.TemporaryDirectory() as temp_dwld_folder:
         for file in os.listdir(temp_dwld_folder):
             if not file.endswith(".whl"):
                 continue
-            with zipfile.ZipFile(
-                os.path.join(temp_dwld_folder, file), "r"
-            ) as zip_ref:
+            with zipfile.ZipFile(os.path.join(temp_dwld_folder, file), "r") as zip_ref:
                 zip_ref.extractall(temp_folder)
 
         # Take every pyi file and move it to "PyQt6-stubs"
@@ -54,9 +51,36 @@ with tempfile.TemporaryDirectory() as temp_dwld_folder:
                         os.path.join(temp_folder, folder, file),
                         os.path.join("PyQt6-stubs", file),
                     )
-                    subprocess.check_call(
-                        ["git", "add", os.path.join("PyQt6-stubs", file)]
-                    )
+                    subprocess.check_call(["git", "add", os.path.join("PyQt6-stubs", file)])
+
+# Apply the quick fixes from mypy:
+for file in os.listdir("PyQt6-stubs"):
+    file_to_fix = os.path.join("PyQt6-stubs", file)
+
+    result = api.run([file_to_fix])[0]
+    if result.startswith("Success"):
+        continue
+
+    with open(file_to_fix, "r", encoding="utf-8") as fhandle:
+        lines = fhandle.readlines()
+
+    for line in result.split("\n"):
+        try:
+            line_nbr = int(line.split(":", 2)[1])
+        except IndexError:
+            # Ignore first line ("Found x errors[...]" and empty last line.
+            continue
+        try:
+            error_msg = line.split("error: ")[1]
+        except IndexError:
+            # Do no parse stuff like notes...
+            continue
+
+        if error_msg == 'Overload does not consistently use the "@staticmethod" decorator on all function signatures.':
+            lines[line_nbr - 1] = lines[line_nbr - 1][:-1] + "  # type: ignore[misc]\n"
+
+    with open(file_to_fix, "w", encoding="utf-8") as fhandle:
+        fhandle.writelines(lines)
 
 # Now apply the fixes:
 for file in os.listdir("PyQt6-stubs"):
@@ -72,5 +96,5 @@ for file in os.listdir("PyQt6-stubs"):
         fhandle.write(modified_tree.code)
 
 # Lint the files with iSort and Black
-subprocess.check_call(["isort", "--profile", "black", "-l 79", "PyQt6-stubs"])
-subprocess.check_call(["black", "--safe", "--quiet", "-l 79", "PyQt6-stubs"])
+subprocess.check_call(["isort", "--profile", "black", "-l 10000", "PyQt6-stubs"])
+subprocess.check_call(["black", "--safe", "--quiet", "-l 10000", "PyQt6-stubs"])
